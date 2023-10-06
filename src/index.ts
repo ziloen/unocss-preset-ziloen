af37a03bf70c3d0a8083f5829abe7f9d65927b30
N <34268416+ziloen@users.noreply.github.com>
2023-10-06 10:38:54 +0800

    chore: add flex rules

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,11 @@ export const presetZiloen = definePreset(() => {
 
       // flex
       ['flex-center', { display: 'flex', 'justify-content': 'center', 'align-items': 'center' }],
-      ['flex-between', { display: 'flex', 'justify-content': 'space-between' }],
       ['flex-align', { display: 'flex', 'align-items': 'center' }],
       ['flex-justify', { display: 'flex', 'justify-content': 'center' }],
+      ['flex-between', { display: 'flex', 'justify-content': 'space-between' }],
+      ['flex-around', { display: 'flex', 'justify-content': 'space-around' }],
+      ['flex-evenly', { display: 'flex', 'justify-content': 'space-evenly' }],
       ['flex-stretch', { display: 'flex', 'align-items': 'stretch' }],
 
       // word-wrap / overflow-wrap
