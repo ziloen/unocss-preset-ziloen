5737b27ca6f2ec3c4fb35e977c4ce0e8c1748944
N <34268416+ziloen@users.noreply.github.com>
2023-12-13 11:44:40 +0800

    chore(test): update test & update deps

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -18,4 +18,12 @@ test('presetStarter', async () => {
     .flex-align{display:flex;align-items:center;}
     .flex-justify{display:flex;justify-content:center;}"
   `)
+
+  const { css: css2 } = await uno.generate('z-max z-min')
+
+  expect(css2).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .z-max{z-index:2147483647;}
+    .z-min{z-index:-2147483648;}"
+  `)
 })
